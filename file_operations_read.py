39d8bade6dfdf63e548edf6fdcb738f7ab0b380a
sofiacarreteroguerrero <sofia.carretero.g@gmail.com>
2025-02-18 13:44:31 +0100

    fixed mistakes

--- file_operations_read.py
+++ file_operations_read.py
@@ -1,13 +1,16 @@
-fp = open("text.txt", "r") #this is open for reading
-print(fp.read()) #read () method gets entire file content
+fp = open("text.txt") # this is open for reading
+print(fp.read()) # read() method gets the entire file content
 fp.close() #this is good practice
 
-#same thing using a context manager
-with open ("text.txt, "r"") as f:
+# same thing using a context manager!
+# this is more pythonic
+with open("text.txt", "r") as f:
     print(f.read())
-#no need to close
+# no need to close
 
-#read the file line by line
-print("now we read it line by line")
-with open ("text.txt", "r") as f:
-    for line in f: #f is iterable
+# read the file line by line
+with open("text.txt", "r") as f:
+    for line in f: # f is iterable and we get each individual line
+        print(f.read())
+        # print(lind, end="")
+        print(line.rstrip())
